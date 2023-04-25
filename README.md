9c3874deae0df96807f92ccd302797f52229e561
spias8 <abullbashser20@gmail.com>
2023-04-26 02:09:31 +0600

    Updated By Me Now!

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # Daily_Ayah
+
+In this application, you can read different types of islamic dowa and also you can download from this application.
--- app/src/main/java/com/pias/dailyayah/MainActivity.java
+++ app/src/main/java/com/pias/dailyayah/MainActivity.java
@@ -2,13 +2,37 @@ package com.pias.dailyayah;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.view.Window;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+
+import com.bumptech.glide.Glide;
 
 public class MainActivity extends AppCompatActivity {
+
+    private ImageView imageView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getSupportActionBar().hide();
         setContentView(R.layout.activity_main);
+
+        imageView= findViewById(R.id.image_view);
+        String url= "https://i.ibb.co/c2tXtyr/d1.jpg";
+        Glide.with(MainActivity.this)
+                .load(url)
+                .centerCrop()
+                .into(imageView);
+    }
+
+    public void nextImage(View view){
+
+    }
+
+    public void previousImage(View view){
+
     }
 }
